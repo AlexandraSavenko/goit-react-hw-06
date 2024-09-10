5e036734f04b70d988271a343eb77cc49922dcee
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-09-10 15:46:40 +0300

    actions added

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,9 +1,12 @@
+import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 export default function ContactList({ contArr, onDelete }) {
+  const phoneBook = useSelector((state) => state.number);
+
   return (
     <ul>
-      {contArr.map((contact) => {
+      {phoneBook.map((contact) => {
         return (
           <li className={css.item} key={contact.id}>
             <Contact contact={contact} onDelete={onDelete} />
